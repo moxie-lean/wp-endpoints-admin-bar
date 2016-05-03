eb7315cfa1f3bdaa6087bc90d842dcdf6314235a
Andres Barrios <andybar2@gmail.com>
2016-05-03 18:20:39 -0300

    Fix indentation.

--- src/AdminBarApi.php
+++ src/AdminBarApi.php
@@ -7,24 +7,24 @@ use Lean\AbstractEndpoint;
  */
 class AdminBarApi extends AbstractEndpoint {
 
-	/**
-	 * Endpoint path
-	 *
-	 * @Override
-	 * @var String
-	 */
-	protected $endpoint = '/admin-bar';
+  /**
+   * Endpoint path
+   *
+   * @Override
+   * @var String
+   */
+  protected $endpoint = '/admin-bar';
 
-	/**
-	 * Get the data.
-	 *
-	 * @Override
-	 * @param \WP_REST_Request $request The request.
-	 *
-	 * @return array|\WP_Error
-	 */
-	public function endpoint_callback( \WP_REST_Request $request ) {
-		$admin_url = admin_url();
+  /**
+   * Get the data.
+   *
+   * @Override
+   * @param \WP_REST_Request $request The request.
+   *
+   * @return array|\WP_Error
+   */
+  public function endpoint_callback( \WP_REST_Request $request ) {
+    $admin_url = admin_url();
     $post_types = get_post_types( array(), 'objects' );
     $post_types_data = array();
 
@@ -46,6 +46,6 @@ class AdminBarApi extends AbstractEndpoint {
       'post_types' => $post_types_data,
     ];
 
-		return $this->filter_data( $data );
-	}
+    return $this->filter_data( $data );
+  }
 }
