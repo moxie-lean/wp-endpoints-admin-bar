213b53a12855b7cf79d2ebb9e6c15fb46ae1b428
Andres Barrios <andybar2@gmail.com>
2016-05-03 18:17:47 -0300

    Add post types and their corresponding creation URL.

--- src/AdminBarApi.php
+++ src/AdminBarApi.php
@@ -24,18 +24,27 @@ class AdminBarApi extends AbstractEndpoint {
 	 * @return array|\WP_Error
 	 */
 	public function endpoint_callback( \WP_REST_Request $request ) {
-		global $wp_admin_bar;
+		$admin_url = admin_url();
+    $post_types = get_post_types( array(), 'objects' );
+    $post_types_data = array();
 
-		$current_user = wp_get_current_user();
+    foreach ($post_types as $type) {
+      if ( $type->show_in_menu === true ) {
+        $post_types_data[] = array(
+          'name' => $type->labels->singular_name,
+          'url' => $admin_url . 'post-new.php?post_type=' . $type->name
+        );
+      }
+    }
 
-		$data = [
-			'site_name' => get_bloginfo( 'name' ),
-			'user_name' => $current_user->user_login,
-			'dashboard_url' => admin_url(),
-			'logout_url' => wp_logout_url(),
-			'edit_page_url' => '',
-			'widgets' => $wp_admin_bar->get_node( 'new-content' ),
-		];
+    $data = [
+      'site_name' => get_bloginfo( 'name' ),
+      'user_name' => '',
+      'dashboard_url' => $admin_url,
+      'logout_url' => wp_logout_url(),
+      'edit_page_url' => '',
+      'post_types' => $post_types_data,
+    ];
 
 		return $this->filter_data( $data );
 	}
